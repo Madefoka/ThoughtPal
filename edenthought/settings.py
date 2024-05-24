792978b7475cc89b938e71f0e91ab101b53f7180
Radek J <raddas@gazeta.pl>
2024-05-24 15:56:12 +0200

    Update settings.py #4

--- edenthought/settings.py
+++ edenthought/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["thoughtpal-django-app.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS=[]
 
